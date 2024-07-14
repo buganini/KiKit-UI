250400e48c4daa320e3a6f94a6d46b973725d988
Buganini Chiu <buganini@b612.tw>
2024-07-15 02:41:05 +0800

    implement mouse dragging

--- ui.py
+++ ui.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env /Applications/KiCad/KiCad.app/Contents/Frameworks/Python.framework/Versions/3.9/bin/python3.9
 from kikit import panelize
 from kikit.units import mm
+from shapely import Point, Polygon
 import pcbnew
 
 import sys
@@ -58,6 +59,8 @@ class UI(Application):
         self.state.mb_diameter = 0.5 * mm
         self.state.mb_spacing = 0.75 * mm
 
+        self.mousehold = False
+
         inputs = sys.argv[1:]
         for boardfile in inputs:
             pcb = PCB(boardfile)
@@ -96,19 +99,24 @@ class UI(Application):
         if len(pcbs) == 0:
             return
 
+        pos_x = 0
+        pos_y = 0
+
         if save:
             output = SaveFile(self.state.output, "KiCad PCB (*.kicad_pcb)")
             if output:
                 if not output.endswith(".kicad_pcb"):
                     output += ".kicad_pcb"
                 self.state.output = output
+            pos_x = pcbs[0].pos_x
+            pos_y = pcbs[0].pos_y
 
         panel = panelize.Panel(self.state.output)
         for pcb in pcbs:
             x1, y1, x2, y2 = pcb.bbox
             panel.appendBoard(
                 pcb.file,
-                pcbnew.VECTOR2I(pcbs[0].pos_x + x1, pcbs[0].pos_y + y1),
+                pcbnew.VECTOR2I(pos_x + x1, pos_y + y1),
                 origin=panelize.Origin.TopLeft,
                 tolerance=panelize.fromMm(1),
                 rotationAngle=pcbnew.EDA_ANGLE(pcb.rotate * 90, pcbnew.DEGREES_T),
@@ -117,7 +125,8 @@ class UI(Application):
 
         panel.buildPartitionLineFromBB()
         cuts = panel.buildFullTabs(cutoutDepth=3*mm)
-        self.state.cuts = cuts
+        if not save:
+            self.state.cuts = cuts
 
         cut_method = self.state.cut_method
         if cut_method == "mb":
@@ -127,29 +136,6 @@ class UI(Application):
 
         if save:
             panel.save()
-
-    def setFocus(self, pcb):
-        self.state.focus = pcb
-
-    def moveUp(self, pcb):
-        pcb.moveUp()
-        self.autoScale()
-        self.build()
-
-    def moveLeft(self, pcb):
-        pcb.moveLeft()
-        self.autoScale()
-        self.build()
-
-    def moveRight(self, pcb):
-        pcb.moveRight()
-        self.autoScale()
-        self.build()
-
-    def moveDown(self, pcb):
-        pcb.moveDown()
-        self.autoScale()
-        self.build()
 
     def rotateCCW(self, pcb):
         pcb.rotate += 1
@@ -161,11 +147,53 @@ class UI(Application):
         self.autoScale()
         self.build()
 
+    def toCanvas(self, x, y):
+        scale, offx, offy = self.state.scale
+        return (offx + x) * scale, (offy + y) * scale
+
+    def fromCanvas(self, x, y):
+        scale, offx, offy = self.state.scale
+        return x/scale - offx, y/scale - offy
+
+    def mousedown(self, e):
+        self.mousepos = e.x, e.y
+        self.mousehold = True
+        self.mousemoved = False
+
+    def mouseup(self, e):
+        self.mousehold = False
+        if not self.mousemoved:
+            pcbs = self.state.pcb
+            x, y = self.fromCanvas(e.x, e.y)
+            p = Point(x, y)
+            for pcb in [pcb for pcb in pcbs if pcb is not self.state.focus]:
+                x1, y1, x2, y2 = pcb.bbox
+                if Polygon([(x1,y1), (x2,y1), (x2,y2), (x1,y2), (x1,y1)]).contains(p):
+                    if self.state.focus is pcb:
+                        continue
+                    else:
+                        self.state.focus = pcb
+        self.build()
+
+    def mousemove(self, e):
+        self.mousemoved = True
+        if self.mousehold:
+            x1, y1 = self.fromCanvas(*self.mousepos)
+            x2, y2 = self.fromCanvas(e.x, e.y)
+            dx = x2 - x1
+            dy = y2 - y1
+            if self.state.focus:
+                self.state.focus.x += int(dx)
+                self.state.focus.y += int(dy)
+                self.state()
+        self.mousepos = e.x, e.y
+
     def drawPCB(self, canvas, pcb, highlight):
         stroke = 0x00FFFF if highlight else 0x0000FF
         x1, y1, x2, y2 = pcb.bbox
-        scale, offx, offy = self.state.scale
-        canvas.drawRect((offx + x1) * scale, (offy + y1) * scale, (offx + x2) * scale, (offy + y2) * scale, stroke=stroke)
+        x1, y1 = self.toCanvas(x1, y1)
+        x2, y2 = self.toCanvas(x2, y2)
+        canvas.drawRect(x1, y1, x2, y2, stroke=stroke)
 
     def painter(self, canvas):
         pcbs = self.state.pcb
@@ -180,22 +208,26 @@ class UI(Application):
                 continue
             self.drawPCB(canvas, pcb, True)
 
-        cut_method = self.state.cut_method
-        mb_diameter = self.state.mb_diameter
-        mb_spacing = self.state.mb_spacing
-        scale, offx, offy = self.state.scale
-        if cut_method == "mb":
-            for line in cuts:
-                i = 0
-                while i * mb_spacing < line.length:
-                    p = line.interpolate(i*mb_spacing)
-                    canvas.drawEllipse((offx + p.x - pcbs[0].pos_x) * scale, (offy + p.y - pcbs[0].pos_y) * scale, mb_diameter/2*scale, mb_diameter/2*scale, stroke=0xFFFF00)
-                    i += 1
-        if cut_method == "vc":
-            for line in cuts:
-                p1 = line.coords[0]
-                p2 = line.coords[-1]
-                canvas.drawLine((offx + p1[0] - pcbs[0].pos_x) * scale, (offy + p1[1] - pcbs[0].pos_y) * scale, (offx + p2[0] - pcbs[0].pos_x) * scale, (offy + p2[1] - pcbs[0].pos_y) * scale, color=0xFFFF00)
+        if not self.mousehold or not self.mousemoved:
+            cut_method = self.state.cut_method
+            mb_diameter = self.state.mb_diameter
+            mb_spacing = self.state.mb_spacing
+            scale, offx, offy = self.state.scale
+            if cut_method == "mb":
+                for line in cuts:
+                    i = 0
+                    while i * mb_spacing < line.length:
+                        p = line.interpolate(i*mb_spacing)
+                        x, y = self.toCanvas(p.x, p.y)
+                        canvas.drawEllipse(x, y, mb_diameter/2*scale, mb_diameter/2*scale, stroke=0xFFFF00)
+                        i += 1
+            if cut_method == "vc":
+                for line in cuts:
+                    p1 = line.coords[0]
+                    p2 = line.coords[-1]
+                    x1, y1 = self.toCanvas(p1[0], p1[1])
+                    x2, y2 = self.toCanvas(p2[0], p2[1])
+                    canvas.drawLine(x1, y1, x2, y2, color=0xFFFF00)
 
     def content(self):
         with Window():
@@ -209,11 +241,7 @@ class UI(Application):
 
                 with Grid():
                     for i,pcb in enumerate(self.state.pcb):
-                        Label(f"{i+1}. {pcb.file}").click(self.setFocus, pcb).grid(row=i, column=0)
-                        Button("Left").click(self.moveLeft, pcb).grid(row=i, column=1)
-                        Button("Up").click(self.moveUp, pcb).grid(row=i, column=2)
-                        Button("Down").click(self.moveDown, pcb).grid(row=i, column=3)
-                        Button("Right").click(self.moveRight, pcb).grid(row=i, column=4)
+                        Label(f"{i+1}. {pcb.file}").grid(row=i, column=0)
                         Button("CCW").click(self.rotateCCW, pcb).grid(row=i, column=5)
                         Button("CW").click(self.rotateCW, pcb).grid(row=i, column=6)
 
@@ -222,7 +250,12 @@ class UI(Application):
                 self.state.cut_method
                 self.state.mb_diameter
                 self.state.mb_spacing
-                Canvas(self.painter).layout(width=self.state.canvas_width, height=self.state.canvas_height).style(bgColor=0x000000)
+                (Canvas(self.painter)
+                    .mousedown(self.mousedown)
+                    .mouseup(self.mouseup)
+                    .mousemove(self.mousemove)
+                    .layout(width=self.state.canvas_width, height=self.state.canvas_height)
+                    .style(bgColor=0x000000))
 
                 Spacer()
 
