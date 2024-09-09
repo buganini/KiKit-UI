6925eb6b7d31e0795ca7d96c43c18620572ee4f1
Buganini Chiu <buganini@b612.tw>
2024-09-09 16:30:54 +0800

    build_exe.py: add macOS dmg build process

--- build_exe.py
+++ build_exe.py
@@ -1,8 +1,30 @@
 import PyInstaller.__main__
+import platform
+import subprocess
+
+create_dmg = False
+
+args = []
+if platform.system()=="Darwin":
+    args.append("--onefile")
+    args.extend(["--add-binary", f"/Applications/KiCad/KiCad.app/Contents/Frameworks/*.dylib:."])
+    create_dmg = True
+else:
+    args.append("--onedir")
+
+print("Args", args)
 
 PyInstaller.__main__.run([
     'kikit-ui.py',
     '--noconfirm',
     '--windowed',
-    '-D'
+    *args
 ])
+
+if create_dmg:
+    subprocess.run([
+        "create-dmg",
+        "--volname", "KiKit-UI",
+        "--app-drop-link", "0", "0",
+        "kikit-ui.dmg", "dist/kikit-ui.app"
+    ])
