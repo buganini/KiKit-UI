65d22db10b3030bc3440f09f9a4ea6aee7ca3872
Buganini Chiu <buganini@b612.tw>
2024-11-09 07:25:53 +0800

    fix resources path

--- kikit-ui.py
+++ kikit-ui.py
@@ -50,7 +50,7 @@ def resource_path(relative_path):
     try:
         base_path = sys._MEIPASS
     except Exception:
-        base_path = os.path.abspath(".")
+        base_path = os.path.abspath("resources")
 
     return os.path.join(base_path, relative_path)
 
