88af55e8200ca19dd5266f4ea9f9d1386ce1078c
Buganini Chiu <buganini@b612.tw>
2024-11-09 08:09:32 +0800

    update build-package.py (codesign+notarize)

--- build-package.py
+++ build-package.py
@@ -2,39 +2,58 @@ import PyInstaller.__main__
 import platform
 import subprocess
 import os
+import itertools
+import glob
 
 import kikit
 kikit_base = os.path.dirname(kikit.__file__)
 
 create_dmg = False
 
-args = []
+pyinstaller_args = []
 create_dmg_args = []
 if platform.system()=="Darwin":
-    args.extend(["--add-binary", f"/Applications/KiCad/KiCad.app/Contents/Frameworks/*.dylib:."])
-    args.extend(["-i", 'icon.icns'])
+    pyinstaller_args.extend(["--add-binary", f"/Applications/KiCad/KiCad.app/Contents/Frameworks/*.dylib:."])
+    pyinstaller_args.extend(["-i", 'resources/icon.icns'])
     subprocess.run(["security", "find-identity", "-v", "-p", "codesigning"])
-    codesign_identity = input("Enter the codesign identity (leave empty for no signing): ").strip()
+    codesign_identity = input("Enter the codesign identity \"Developer ID Application: XXXXXX (XXXXXXXXXX)\" (leave empty for no signing): ").strip()
     if codesign_identity:
-        args.extend(["--codesign-identity", codesign_identity])
         create_dmg_args.extend(["--codesign", codesign_identity])
         create_dmg_args.extend(["--notarize", "notarytool-creds"])
     create_dmg = True
 else:
-    args.extend(["-i", 'icon.ico'])
+    pyinstaller_args.extend(["-i", 'resources/icon.ico'])
 
-args.extend(["--add-data", f"{os.path.join(kikit_base, 'resources', 'kikit.pretty')}:kikit.pretty"])
+pyinstaller_args.extend(["--add-data", f"{os.path.join(kikit_base, 'resources', 'kikit.pretty')}:kikit.pretty"])
 
-print(args)
+print(pyinstaller_args)
 
 PyInstaller.__main__.run([
     'kikit-ui.py',
     "--onedir",
-    '--noconfirm',
-    '--windowed',
-    '--add-data=icon.ico:.',
-    *args
+    "--noconfirm",
+    "--windowed",
+    "--add-data=resources/icon.ico:.",
+    *pyinstaller_args
 ])
+
+if codesign_identity:
+    for path in itertools.chain(
+        glob.glob("dist/kikit-ui.app/**/*.so", recursive=True),
+        glob.glob("dist/kikit-ui.app/**/*.dylib", recursive=True),
+        glob.glob("dist/kikit-ui.app/**/Python3", recursive=True),
+        ["dist/kikit-ui.app"],
+    ):
+        print("codesign", path)
+        subprocess.run(["codesign",
+            "--sign", codesign_identity,
+            "--entitlements", "resources/entitlements.plist",
+            "--timestamp",
+            "--deep",
+            str(path),
+            "--force",
+            "--options", "runtime"
+        ])
 
 if create_dmg:
     if os.path.exists("kikit-ui.dmg"):
@@ -42,6 +61,7 @@ if create_dmg:
     subprocess.run([
         "create-dmg",
         "--volname", "KiKit-UI",
+        "--volicon", "resources/icon.icns",
         "--app-drop-link", "0", "0",
         *create_dmg_args,
         "kikit-ui.dmg", "dist/kikit-ui.app"
